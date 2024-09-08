be343ac7264dd4ae41ad6eef8308cb13f0ab3825
AntonLosaria <qjapflosaria@tip.edu.ph>
2024-09-08 19:20:39 +0800

    Add files via upload

---
+++ Sept4.cpp
@@ -0,0 +1,21 @@
+/*#include<stdio.h>
+int main () {
+
+ 	 simple hello world 
+ 
+	printf("Hello, World");
+	
+	return 0;
+	
+}
+*/
+/*
+#include<iostream>
+using namespace std;
+
+int main () {
+	
+	cout << "Hello, World" << endl;
+	return 0;
+	
+} */
